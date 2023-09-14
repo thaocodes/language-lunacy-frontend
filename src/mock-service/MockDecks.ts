234250e4b1f98680f5a5f8610a983561bb379837
thao <thaole0325@gmail.com>
2023-09-14 19:32:05 -0400

    fixed mock deck data to represent a single deck

--- src/mock-service/MockDecks.ts
+++ src/mock-service/MockDecks.ts
@@ -1,29 +1,32 @@
-import { Flashcard } from './types';
+import { Deck } from '../components/types';
 
-export const mockDecks: Flashcard[] = [
-    {
-        id: 1,
-        "number": 1,
-        language: "Dutch",
-        "dutch": "als",
-        "english": "as",
-        "difficulty": "unset",
-    },
-    {
-        id: 2,
-        "number": 2,
-        language: "Dutch",
-        "dutch": "I",
-        "english": "I",
-        "difficulty": "unset",
-    },
-    {
-        id: 3,
-        "number": 3,
-        language: "Dutch",
-        "dutch": "zijn",
-        "english": "his",
-        "difficulty": "unset",
-    }
-]
-
+export const mockDeck: Deck = {
+    id: 1,
+    name: "Dutch Deck",
+    flashcards: [
+        {
+            id: 1,
+            number: 1,
+            language: "dutch",
+            english: "as",
+            difficulty: "unset",
+            dutch: "als",
+        },
+        {
+            id: 2,
+            number: 2,
+            language: "dutch",
+            english: "I",
+            difficulty: "unset",
+            dutch: "I",
+        },
+        {
+            id: 3,
+            number: 3,
+            language: "dutch",
+            english: "his",
+            difficulty: "unset",
+            dutch: "zijn",
+        }
+    ]
+};
