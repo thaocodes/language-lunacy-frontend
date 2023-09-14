405cdcf30b2c21cf20dee0133e939fcd6bfed87a
thao <thaole0325@gmail.com>
2023-09-14 14:58:39 -0400

    mock data for placeholder

---
+++ src/mock-service/MockDecks.ts
@@ -0,0 +1,29 @@
+import { Flashcard } from './types';
+
+export const mockDecks: Flashcard[] = [
+    {
+        id: 1,
+        "number": 1,
+        language: "Dutch",
+        "dutch": "als",
+        "english": "as",
+        "difficulty": "unset",
+    },
+    {
+        id: 2,
+        "number": 2,
+        language: "Dutch",
+        "dutch": "I",
+        "english": "I",
+        "difficulty": "unset",
+    },
+    {
+        id: 3,
+        "number": 3,
+        language: "Dutch",
+        "dutch": "zijn",
+        "english": "his",
+        "difficulty": "unset",
+    }
+]
+
