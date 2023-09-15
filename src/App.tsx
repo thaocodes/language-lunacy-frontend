2f97289bd0d4952862431f825091aa67e1744d05
thao <thaole0325@gmail.com>
2023-09-15 12:34:43 -0400

    API call to endpoint to get flashcards in specified language & optional start/end params working

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,42 @@ import { mockDeck } from './mock-service/MockDecks';
 import { Deck } from './components/types';
 import FlashcardDeck from './components/FlashcardDeck';
 import InputField from './components/InputField';
+import axios from 'axios'; 
+
 
 const App: React.FC = () => {
     const [language, setLanguage] = useState<string>("");
+    const [start, setStart] = useState<string>("");
+    const [end, setEnd] = useState<string>("");
     const [deck, setDeck] = useState<Deck>(mockDeck);  // 1 deck, set initial state to mockDeck
+    const [error, setError] = useState<string>("");
 
     const handleLanguage = (e: React.FormEvent) => {
         e.preventDefault();
 
-        if(language) {
-            if (language.toLowerCase() === mockDeck.flashcards[0].language) {
-                setDeck(mockDeck);   // set state to mock deck data
-                console.log(mockDeck);
-            }
+        console.log("baseURL: ", process.env.REACT_APP_API_BASE_URL);
+        
+        axios.get(`${process.env.REACT_APP_API_BASE_URL}/${language}/${start}/${end}`)
+            
+            .then(response => {
+                setDeck(response.data);
+                setError("");    // reset error state if successful response
+                console.log("RESPONSE: ", response);
+                console.log("RESPONSE.DATA: ", response.data);
+            })
+            .catch(error => {
+                // extract error message from response & set it to error state
+                setError(error.response?.data?.error || "An unexpected error occured")
+                console.log("Error fetching data!", error)
+            });
             console.log(language);
 
-            setLanguage("");    // reset language state 
-        }
+            // reset fields after submitting
+            setLanguage("");    
+            setStart("");
+            setEnd("");
     }
-    
+
 
 
     return (
@@ -30,9 +47,15 @@ const App: React.FC = () => {
             <span className="heading">Language Lunacy</span>
             <InputField 
                 language={language}
+                start={start}
+                end={end}
                 setLanguage={setLanguage}
+                setStart={setStart}
+                setEnd={setEnd}
                 handleLanguage={handleLanguage}
             />
+            {/* display error message if there is one */}
+            {error && <div className="error-message">{error}</div>}
             <FlashcardDeck deck={deck} />
         </div>
     );
