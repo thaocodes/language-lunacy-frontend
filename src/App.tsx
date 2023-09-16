67c5a3940b521c2c2d5a8c73839457a536cb9727
thao <thaole0325@gmail.com>
2023-09-16 16:32:26 -0400

    refactored App to create new deck when language inputted, created states for selected deck & deck list, Sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React, { useState } from 'react';
 import './App.css';
-import { mockDeck } from './mock-service/MockDecks';
 import { Deck } from './components/types';
 import SingleDeck from './components/SingleDeck';
 import InputField from './components/InputField';
 import axios from 'axios'; 
+import Sidebar from './components/Sidebar';
 
 
 const App: React.FC = () => {
     const [language, setLanguage] = useState<string>("");
     const [start, setStart] = useState<string>("");
     const [end, setEnd] = useState<string>("");
-    const [deck, setDeck] = useState<Deck>(mockDeck);  // 1 deck, set initial state to mockDeck
     const [error, setError] = useState<string>("");
-
-    const handleLanguage = (e: React.FormEvent) => {
+    const [selectedDeck, setSelectedDeck] = useState<Deck | null>(null); // no deck selected yet
+    const [deckList, setDeckList] = useState<Deck[]>([]);
+    
+    const createDeck = (e: React.FormEvent) => {
         e.preventDefault();
         
         axios.get(`${process.env.REACT_APP_API_BASE_URL}/${language}/${start}/${end}`)
             
             .then(response => {
-                setDeck(response.data);
+                const newDeck: Deck = {
+                    id: Date.now(),   // generate unique id
+                    name: language,
+                    flashcards: response.data   // response data is array of flashcards
+                };
+
+                console.log("Deck: ", newDeck)
+                // add new deck to decklist
+                // updates `decklist` state by using previous state `oldDeckList` to create a new state
+                // `oldDeckList` = param name for callback we're passing to `setDeckList`
+                // represents current state of `deckList` before this update is applied
+                setDeckList(oldDeckList => [...oldDeckList, newDeck]);
                 setError("");    // reset error state if successful response
                 console.log("RESPONSE: ", response);
                 console.log("RESPONSE.DATA: ", response.data);
@@ -37,8 +49,14 @@ const App: React.FC = () => {
             setLanguage("");    
             setStart("");
             setEnd("");
-        }
+        };
 
+        // helper that sets the selected deck
+        // deck is a param of type `Deck` that function gets called with
+        // gets set as selectedDeck when `setSelectedDecl(Deck)` is called 
+        const handleSelectDeck = (deck: Deck) => {
+            setSelectedDeck(deck)
+        }
 
     return (
         <div className="App">
@@ -50,11 +68,18 @@ const App: React.FC = () => {
                 setLanguage={setLanguage}
                 setStart={setStart}
                 setEnd={setEnd}
-                handleLanguage={handleLanguage}
+                createDeck={createDeck}
             />
             {/* if error truthy, display error message */}
             {error && <div className="error-message">{error}</div>}
-            <SingleDeck deck={deck} />
+            <SingleDeck 
+                selectedDeck={selectedDeck}
+                setSelectedDeck={setSelectedDeck}
+            />
+            <Sidebar
+                deckList={deckList}
+                handleSelectDeck={handleSelectDeck}
+            />
         </div>
     );
 }
