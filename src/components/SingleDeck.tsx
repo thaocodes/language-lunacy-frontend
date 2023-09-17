9494fd936754f18b4ae4dba6615d4208cdc1db34
thao <thaole0325@gmail.com>
2023-09-17 10:40:15 -0400

    selecting and deleting a deck working

--- src/components/SingleDeck.tsx
+++ src/components/SingleDeck.tsx
@@ -1,14 +1,42 @@
 import React from 'react'
 import { Deck } from './types';
+import SingleFlashcard from './SingleFlashcard';
 
-type Props = {
-    deck: Deck;
+type DeckProps = {
+    selectedDeck: Deck | null;
+    decklist: Deck[];
+    currentFlashcardIndex: number;
+    setSelectedDeck: React.Dispatch<React.SetStateAction<Deck | null>>;
+    onEasy: () => void;
+    onHard: () => void;
+    nextFlashcard: () => void;
 }
 
-const SingleDeck: React.FC<Props> = ({ deck }) => {
-    return (
-        <div>A Deck of Flashcards</div>
-    )
+const SingleDeck: React.FC<DeckProps> = ({ selectedDeck, decklist, currentFlashcardIndex, setSelectedDeck, onEasy, onHard, nextFlashcard  }) => {
+
+    if (selectedDeck) {
+        const currentFlashcard = selectedDeck.flashcards[currentFlashcardIndex];
+
+        // check if currentFlashcardIndex within bounds
+        if (currentFlashcardIndex >= 0 && currentFlashcardIndex < selectedDeck.flashcards.length) {
+
+            return (
+                <>
+                    {/* flashcard counter */}
+                    <div> Card {currentFlashcardIndex + 1 } of {selectedDeck.flashcards.length}</div>
+                    <SingleFlashcard
+                        flashcard={currentFlashcard}
+                        onEasy={onEasy}
+                        onHard={onHard}
+                    />
+                </>
+            );
+        } else {
+            return <div>No more flashcards!</div>;
+        } 
+    } else {
+        return <div>Select a deck to start</div>;
+    }
 };
 
 export default SingleDeck;
