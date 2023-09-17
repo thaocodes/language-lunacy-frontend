b4fb1ff1bf82eeaae0cef9e8918006c3cec1b077
thao <thaole0325@gmail.com>
2023-09-17 10:40:43 -0400

    each deck renders a delete button, delete action working

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,26 +1,40 @@
 import React from 'react'
 import { Deck } from './types';
+import '../stylesheets/flashcard.css';
 
 type Props = {
     deckList: Deck[];
+    selectedDeck: Deck | null;
     handleSelectDeck: (deck: Deck) => void;
+    setDecklist: React.Dispatch<React.SetStateAction<Deck[]>>;
 }
 
 // map over deckList to display each deck
 // when a deck is clicked, handleSelectDeck will be called with the clicked deck as the arg
 // to select that deck
 
-const Sidebar: React.FC<Props> = ({ deckList, handleSelectDeck }) => {
+const Sidebar: React.FC<Props> = ({ deckList, selectedDeck, handleSelectDeck, setDecklist }) => {
+     // if deck.id does not match id, return it
+    const deleteDeck = (id: number) => {
+        setDecklist(deckList.filter((deck) => deck.id !== id));
+    };
+
     return (
         <div className="sidebar">
             <h2>Decks</h2>
-            {deckList.map((deck, index) => (
-                <div key={index} onClick={() => handleSelectDeck(deck)}>
-                    {deck.name}
-                </div>
+            {deckList.map((deck) => (
+                <div key={deck.id} className="deck-item">
+                    <div className="deck-name" onClick={() => handleSelectDeck(deck)}>
+                        {deck.name} ({deck.flashcards.length} cards)
+                    </div>
+                    {/* prevent clicking delete button from also triggering `handleSelectDeck` */}
+                <button className="delete-button" onClick={(e) => { e.stopPropagation(); deleteDeck(deck.id) }}>
+                    Delete
+                </button>
+            </div>
             ))}
         </div>
-    )
+    );
 };
 
 export default Sidebar;
