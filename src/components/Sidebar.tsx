423976ccc4dafe1381f54926c00ec097c89ed623
thao <thaole0325@gmail.com>
2023-09-21 22:03:56 -0400

    working on styling for sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Deck } from './types';
-import '../stylesheets/flashcard.css';
+import '../stylesheets/sidebar.css';
+import { MDBBtn, MDBIcon, MDBListGroup, MDBListGroupItem } from 'mdb-react-ui-kit';
 
 type Props = {
     deckList: Deck[];
@@ -18,18 +19,25 @@ const Sidebar: React.FC<Props> = ({ deckList, handleSelectDeck, setDecklist }) =
     return (
         <div className="sidebar">
             <h2>Decks</h2>
-            {deckList.map((deck) => (
-                <div key={deck.id} className="deck-item">
-                     {/* when deck is clicked, handleSelectDeck will be called w/ the clicked deck as arg */}
-                    <div className="deck-name" onClick={() => handleSelectDeck(deck)}>
-                        {deck.name} ({deck.flashcards.length} cards)
-                    </div>
-                    {/* prevent clicking delete button from also triggering `handleSelectDeck` */}
-                <button className="delete-button" onClick={(e) => { e.stopPropagation(); deleteDeck(deck.id) }}>
-                    Delete
-                </button>
-            </div>
-            ))}
+            <MDBListGroup className="list-group-flush">
+                {deckList.map((deck) => (
+                    // when deck is clicked, handleSelectDeck will be called w/ the clicked deck as arg 
+                    <MDBListGroupItem key={deck.id} className="deck-item" onClick={() => handleSelectDeck(deck)}>
+                        <span>{deck.name} ({deck.flashcards.length} cards)</span>
+                        {/* prevent clicking delete button from also triggering `handleSelectDeck` */}
+                        <MDBBtn 
+                            floating 
+                            tag='a' 
+                            color='danger'
+                            size='sm'
+                            className='ml-1'
+                            onClick={(e) => { e.stopPropagation(); deleteDeck(deck.id) }}
+                        >
+                            <MDBIcon fas icon='trash' /> 
+                        </MDBBtn>
+                    </MDBListGroupItem>
+                ))}
+            </MDBListGroup>
         </div>
     );
 };
