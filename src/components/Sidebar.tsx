fc46d342b679284b82f6e7c0a3250bcaf794d762
thao <thaole0325@gmail.com>
2023-10-06 13:45:03 -0400

    updated comments

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,9 +12,10 @@ type Props = {
 
 // map over deckList to display each deck
 const Sidebar: React.FC<Props> = ({ deckList, handleSelectDeck, setDecklist }) => {
-     // if deck.id does not match id, return it
+     // filters deckList to exclude deck with the given `id`
     const deleteDeck = (id: number) => {
-        setDecklist(deckList.filter((deck) => deck.id !== id));
+        // if deck.id does not match id, return it
+        setDecklist(deckList.filter((deck) => deck.id !== id)); 
     };
 
     // takes a string, uppercases first letter 
@@ -27,17 +28,23 @@ const Sidebar: React.FC<Props> = ({ deckList, handleSelectDeck, setDecklist }) =
             <div className="sidebar-header">
                 <h2>Decks</h2>
                 <div className="deck-instruction">
+                    {/* if theres at least 1 deck, display 'Select a Deck'
+                        otherwise, display 'Create a Deck!' */}
                     {deckList.length > 0 ? 'Select a Deck to Start' : 'Create a Deck!'}
                 </div>
                 <ul className="deck-list">
+                    {/* map takes a function & calls it on every item in the array */}
                     {deckList.map((deck) => (
-                        // when deck is clicked, handleSelectDeck will be called w/ the clicked deck as arg 
+                        // for every deck in deckList, create a list item
+                        // when deck is clicked, handleSelectDeck function is called w/ the clicked deck as arg 
                         <li key={deck.id} className="deck-item" onClick={() => handleSelectDeck(deck)}>
+                            {/* for each deck, display name, card count, delete button */}
                             <div className="deck-name">{capitalizeDeckName(deck.name)}</div>
                             <div className="card-count">({deck.flashcards.length} cards)</div>
-                            {/* prevent clicking delete button from also triggering `handleSelectDeck` */}
                             <button 
                                 className="delete-button"
+                                // prevent clicking delete button from also triggering `handleSelectDeck` 
+                                // when clicked, calls deleteDeck func for that deck 
                                 onClick={(e) => { e.stopPropagation(); deleteDeck(deck.id) }}
                             > 
                                 <FaTrash />  {/* trash icon */}
